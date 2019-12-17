9ad63dff1b9fa392323a631f89fc85c98e998e2c
RealJohnGalt <johngalt@fakemail.com>
2019-12-17 08:08:50 -0800

    cpumask: we only have two big cores

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2276,7 +2276,7 @@ EXPORT_SYMBOL(__cpu_isolated_mask);
  * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
  * little and CPUs 4-7 would be big).
  */
-#define LITTLE_CPU_MASK ((1UL << (NR_CPUS / 2)) - 1)
+#define LITTLE_CPU_MASK ((1UL << (NR_CPUS / 2)) + 1)
 #define BIG_CPU_MASK    (((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
 static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
 const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
